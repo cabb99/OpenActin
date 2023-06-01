a171f4caefb80f1e524b436f31069dc928d1c1de
Nusayba El-Ali <nusaybaee@gmail.com>
2023-06-01 15:38:54 -0500

    Adds comment

--- coarseactin/Scene.py
+++ coarseactin/Scene.py
@@ -3,7 +3,7 @@ import numpy as np
 import io
 
 """
-Python library to allow easy handling of coordinate files for molecular dynamics using pandas DataFrames.
+Python library to allow easy handling of coordinate files for molecular dynamics using pandas DataFrames. okay
 """
 if __name__ == "__main__":
     import utils
