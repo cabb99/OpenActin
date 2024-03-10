3eee42bc3a12c747af90b3941be80aa4997b9121
Carlos Bueno <cabb99@gmail.com>
2024-03-09 18:41:46 -0600

    Adds numba functions to calculate dcorr

--- coarseactin/MDFit/mdfit.py
+++ coarseactin/MDFit/mdfit.py
@@ -190,6 +190,90 @@ class MDFit:
         assert np.allclose(self.dsim_map()['dz'],self.dsim_map_numerical()['dz'])
         assert np.allclose(self.dcorr_coef(),self.dcorr_coef_numerical())
 
+from numba import jit
+@jit(nopython=True)
+def fold_outer_mult4(x,y,z,padding):
+    x_reshaped = x.reshape(x.shape[0], x.shape[1], 1, 1)
+    y_reshaped = y.reshape(y.shape[0], 1, y.shape[1], 1)
+    z_reshaped = z.reshape(z.shape[0], 1, 1, z.shape[1])
+    
+    # Perform element-wise multiplication with broadcasting
+    vp = x_reshaped * y_reshaped * z_reshaped
+    p = padding
+
+    vp[:, -2*p:-p, :, :] += vp[:, :p, :, :]
+    vp[:, :, -2*p:-p, :] += vp[:, :, :p, :]
+    vp[:, :, :, -2*p:-p] += vp[:, :, :, :p]
+    vp[:, p:2*p, :, :]   += vp[:, -p:, :, :]
+    vp[:, :, p:2*p, :]   += vp[:, :, -p:, :]
+    vp[:, :, :, p:2*p]   += vp[:, :, :, -p:]
+    vp = vp[:, p:-p, p:-p, p:-p]
+    return vp
+
+@jit(nopython=True)
+def numba_erf(x):
+    # Preallocate the result array with the same shape as x
+    result = np.empty(x.shape, dtype=np.float64)
+    # Iterate over all elements in x by their indices
+    for index, value in np.ndenumerate(x):
+        # Apply math.erf element-wise
+        result[index] = math.erf(value)
+    return result
+    
+
+def dcorr(coordinates,voxel_limits,sigma, experimental_map,padding):
+    sigma=self.sigma*np.sqrt(2)
+
+    x_mu_sigma=(voxel_limits[0]-coordinates[:,None,0])/sigma[0]
+    y_mu_sigma=(voxel_limits[1]-coordinates[:,None,1])/sigma[1]
+    z_mu_sigma=(voxel_limits[2]-coordinates[:,None,2])/sigma[2]
+    
+    phix=(1+numba_erf(x_mu_sigma))/2
+    phiy=(1+numba_erf(y_mu_sigma))/2
+    phiz=(1+numba_erf(z_mu_sigma))/2
+    
+    dphix_dx= -np.exp(-x_mu_sigma**2) / np.sqrt(np.pi) / sigma[0]
+    dphiy_dy= -np.exp(-y_mu_sigma**2) / np.sqrt(np.pi) / sigma[1]
+    dphiz_dz= -np.exp(-z_mu_sigma**2) / np.sqrt(np.pi) / sigma[2]
+    
+    dphix_ds= x_mu_sigma*dphix_dx*np.sqrt(2)
+    dphiy_ds= y_mu_sigma*dphiy_dy*np.sqrt(2)
+    dphiz_ds= z_mu_sigma*dphiz_dz*np.sqrt(2)
+
+    dphix=(phix[:,1:]-phix[:,:-1])
+    dphiy=(phiy[:,1:]-phiy[:,:-1])
+    dphiz=(phiz[:,1:]-phiz[:,:-1])
+    
+    ddphix_dx=dphix_dx[:,1:]-dphix_dx[:,:-1]
+    ddphiy_dy=dphiy_dy[:,1:]-dphiy_dy[:,:-1]
+    ddphiz_dz=dphiz_dz[:,1:]-dphiz_dz[:,:-1]
+    
+    ddphix_ds=dphix_ds[:,1:]-dphix_ds[:,:-1]
+    ddphiy_ds=dphiy_ds[:,1:]-dphiy_ds[:,:-1]
+    ddphiz_ds=dphiz_ds[:,1:]-dphiz_ds[:,:-1]
+
+    smap     = fold_outer_mult4(dphix, dphiy, dphiz, padding)
+    smap_dx  = fold_outer_mult4(ddphix_dx, dphiy, dphiz, padding)
+    smap_dy  = fold_outer_mult4(dphix, ddphiy_dy, dphiz, padding)
+    smap_dz  = fold_outer_mult4(dphix, dphiy, ddphiz_dz, padding)
+    smap_dsx = fold_outer_mult4(ddphix_ds, dphiy, dphiz, padding)
+    smap_dsy = fold_outer_mult4(dphix, ddphiy_ds, dphiz, padding)
+    smap_dsz = fold_outer_mult4(dphix, dphiy, ddphiz_ds, padding)
+
+    
+    dsim=np.array([smap_dx,smap_dy,smap_dz,smap_dsx,smap_dsy,smap_dsz])
+    sim=smap.sum(axis=0)
+    exp=experimental_map
+    
+    num1=np.sum(dsim*exp[None,None,:,:,:],axis=(2,3,4))
+    den1=np.sqrt(np.sum(sim**2)) * np.sqrt(np.sum(exp**2))
+    
+    num2 = np.sum(dsim*sim[None,None,:,:,:],axis=(2,3,4))* np.sum(sim * exp)
+    den2 = np.sum(sim**2) * den1
+    
+    # Final equation
+    return ((num1 / den1) - (num2 / den2))[:,:3]
+
 coordinates=np.random.rand(100,3)*(10,20,5)
 experimental_map=np.random.rand(10,20,5)
 
